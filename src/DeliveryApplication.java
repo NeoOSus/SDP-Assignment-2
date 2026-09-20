7a03438665e5872b29bc08081b14d7615118e1d4
Erkebulan <251288@astanait.edu.kz>
2026-09-20 17:00:58 +0500

    created Delivery Application

---
+++ src/DeliveryApplication.java
@@ -0,0 +1,17 @@
+public class DeliveryApplication {
+    private final Button button;
+    private final Checkbox checkbox;
+    private final Logistics logistics;
+
+    public DeliveryApplication(GUIfactory factory, Logistics logistics) {
+        this.button = factory.createButton();
+        this.checkbox = factory.createCheckbox();
+        this.logistics = logistics();
+    }
+
+    public void run() {
+        button.paint();
+        checkbox.paint();
+        logistics.planDelivery("Furniture", "Aktau");
+    }
+}
